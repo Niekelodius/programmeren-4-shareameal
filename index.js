e4f966cc7700ed5b5296db17d85da2d0510de28f
Niek G <nca.goossens@student.avans.nl>
2022-04-26 09:40:27 +0200

    message

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const router = require('./src/routes/user.routes');
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-let database = [];
-let id = 0;
+
 
 app.all("*", (req, res, next) => {
   const method = req.method;
@@ -21,118 +21,8 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/api/user", (req, res) => {
-  let user = req.body;
-  user = {
-    id,
-    ...user,
-  };
+app.use(router);
 
-  const emailCheck = database.some(element => {
-    if (element.email ===user.email) {
-      return true;
-    }
-  });
-
-  if (emailCheck) {
-    console.log(user.email + " already in use");
-    res.status(403).json({
-      status: 403,
-      result: `email already in use`,
-    });
-  }
-  else{
-    id++;
-    database.push(user);
-    console.log("added " + user);
-    res.status(201).json({
-      status: 201,
-      result: database,
-    });
-  }
-});
-
-
-app.put("/api/user/:userId", (req, res) => {
-  const userId = req.params.userId;
-
-  let oldUser = database.filter((item) => item.id == userId);
-  if (oldUser.length > 0){
-    
-    let newUser = req.body;
-    id = parseInt(userId);
-    newUser = {
-      id,
-      ...newUser,
-    };
-    console.log(newUser);
-    database[userId] = newUser;
-    
-    res.status(201).json({
-      status: 201,
-      result: newUser,
-    });
-    console.log(`Modified user ${userId}`);
-  }
-  else {
-    res.status(403).json({
-      status: 403,
-      result: `User with ID ${userId} not found`,
-    });
-  }
-
-});
-
-app.get("/api/user/profile", (req, res) => {
-  res.status(200).json({
-    status: 401,
-    result: `Unauthorized`,
-  });
-});
-
-app.get("/api/user/:userId", (req, res, next) => {
-  const userId = req.params.userId;
-  console.log(`User met ID ${userId} gezocht`);
-  let user = database.filter((item) => item.id == userId);
-  if (user.length > 0) {
-    console.log(user);
-    res.status(201).json({
-      status: 201,
-      result: user,
-    });
-  } else {
-    res.status(403).json({
-      status: 403,
-      result: `User with ID ${userId} not found`,
-    });
-  }
-});
-
-app.delete("/api/user/:userId", (req, res, next) => {
-  const userId = req.params.userId;
-  console.log(`User with ID ${userId} has been deleted`);
-  let user = database.filter((item) => item.id == userId);
-  if (user.length > 0) {
-    console.log(user);
-    database.splice(userId,1);
-    res.status(201).json({
-      status: 201,
-      result: user,
-    });
-  } else {
-    res.status(403).json({
-      status: 403,
-      result: `User with ID ${userId} not found`,
-    });
-  }
-});
-
-app.get("/api/user", (req, res, next) => {
-  res.status(201).json({
-    status: 201,
-    result: database,
-  });
-});
 
 app.all("*", (req, res) => {
   res.status(401).json({
