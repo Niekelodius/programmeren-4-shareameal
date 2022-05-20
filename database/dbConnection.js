7a0467f3428c404ef2fa7f29ab5c43c8c35f0d92
Niek G <nca.goossens@student.avans.nl>
2022-05-20 19:32:28 +0200

    typo

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const mysql = require('mysql12');
+const mysql = require('mysql2');
 const logger = require('../src/config/config').logger
 
 const pool = mysql.createPool({
