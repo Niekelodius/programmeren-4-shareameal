6b03298faa344f2adef6dfdce7790f6edaceaac0
Niek G <nca.goossens@student.avans.nl>
2022-05-21 20:23:08 +0200

    did i do it

--- src/test/integration/user.controller.test.js
+++ src/test/integration/user.controller.test.js
@@ -13,19 +13,19 @@ const res = require("express/lib/response");
 chai.should();
 chai.use(chaiHttp);
 
-let userId = 5;
+let userId;
 
 let validToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTY1MzA1NDQwMiwiZXhwIjoxNjU0MDkxMjAyfQ.qjG8JF3E-usyhLJCg02mDoKmtO3V36dPUH4vgJxfCIA";
 const invalidToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIyNywiaWF0IjoxNjUyNzg3NzA4LCJleHAiOjE2NTM4MjQ1MDh9.NAW7Ol_7WrEdPYH1B7-6mKFsGGpX3xPwEQBctIKlPvU";
 
-const CLEAR_DB = "DELETE  FROM `user` WHERE emailAdress = 'ng@avans.nl';";
-const GET_USER = "SELECT id FROM `user` WHERE emailAdress = 'goos@avans.nl';";
+const CLEAR_DB = "DELETE  FROM `user` WHERE emailAdress = 'ng@avans.nl'";
+const GET_USER = "SELECT id FROM `user` WHERE emailAdress = 'goos@avans.nl'";
 const ADD_USER =
   "INSERT INTO `user`" +
   "(`firstName`, `lastName`, `street`, `city`, `password`, `emailAdress`, `phoneNumber`,`roles` )" +
-  "VALUES ('Removable', 'man', 'behind', 'you', 'D389!!ach', 'goos@avans.nl', '05322222222', 'editor');  ";
+  "VALUES ('Removable', 'man', 'behind', 'you', 'D389!!ach', 'goos@avans.nl', '05322222222', 'editor')  ";
 
 // {
 //   "id": 3,
@@ -357,35 +357,22 @@ describe("Manage users /api/user", () => {
     
     beforeEach((done) => {
       logger.debug("beforeEach called");
-      // maak de testdatabase leeg zodat we onze testen kunnen uitvoeren.
+      // // maak de testdatabase leeg zodat we onze testen kunnen uitvoeren.
+      // let user = {
+      //   firstName: "Niek",
+      //   lastName: "Goossens",
+      //   emailAdress: "ncag@gmail.com",
+      //   password: "wachtwoord",
+      //   isActive: 1,
+      //   phoneNumber: "0123456789",
+      //   roles: "editor",
+      //   street: "achter",
+      //   city: "dorp"
+      // };
       database.getConnection(function (err, connection) {
-
-        // Use the connection
-        connection.query(
-           CLEAR_DB
-           +
-           'INSERT INTO `user` (id, firstName, lastName, street, city, password, emailAdress, phoneNumber,roles, isActive) VALUES(?, ?,?, ?,?, ?,?, ?, ?);  ' ,
-           [
-             5,
-            "Removeable",
-            "man",
-            "behind",
-            "you",
-            "D389!!ach",
-            "goos@avans.nl",
-            "05322222222",
-            "editor",
-            1
-
-           ]
-          ,
-          function (error, results) {
-            // When done with the connection, release it.
-            // Handle error after the release.
-            // Let op dat je done() pas aanroept als de query callback eindigt!
-            logger.debug("beforeEach done");
-          }
-        );
+        connection.query('INSERT INTO user VALUES(123, "Voor", "Achter", 1, "goos@avans.nl", "123", "0651234567", "editor", "Niek", "G");', (err, result) => {
+          connection.release();
+      })
         connection.query(
           GET_USER,
           function (error, results) {
@@ -394,7 +381,9 @@ describe("Manage users /api/user", () => {
 
             // When done with the connection, release it.
             connection.release();
-
+            if (error) {
+              logger.warn("2"+error);
+            }
             // Handle error after the release.
             // Let op dat je done() pas aanroept als de query callback eindigt!
             logger.debug("beforeEach done");
@@ -402,6 +391,9 @@ describe("Manage users /api/user", () => {
           }
         );
       });
+      
+
+
     });
     it("205-1 Verplicht veld emailAdress", (done) => {
       chai
